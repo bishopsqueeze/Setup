731ce0c5026c5879c2662b3806c1dbdcf6a55ce6
Alex Stephens <bishop.squeeze@gmail.com>
2018-05-28 15:18:56 -0400

    Updated build instructions

--- TaskViewInstall.r
+++ TaskViewInstall.r
@@ -45,54 +45,103 @@
 	library(ctv)
 
 ## [8] Attempt to install views (only use binaries)
-ctv::install.views("HighPerformanceComputing")
+	ctv::install.views("HighPerformanceComputing")
 
-	## attempt to install Rmpi after this error message
-	ERROR: dependency ‘Rmpi’ is not available for package ‘doMPI’
-	* removing ‘/Library/Frameworks/R.framework/Versions/3.5/Resources/library/doMPI’
+		## attempt to install Rmpi after this error message
+		ERROR: dependency ‘Rmpi’ is not available for package ‘doMPI’
+		* removing ‘/Library/Frameworks/R.framework/Versions/3.5/Resources/library/doMPI’
 
-		## attempt to install Rmpi from source
-		install.packages("Rmpi")
-		configure: error: "Cannot find mpi.h header file"
+			## attempt to install Rmpi from source
+			install.packages("Rmpi")
+			configure: error: "Cannot find mpi.h header file"
 
-		## follow instructions on the following website to download
-		## and install OpenMPI (Message Passing Interface)
-		https://wiki.helsinki.fi/display/HUGG/Open+MPI+install+on+Mac+OS+X
+			## follow instructions on the following website to download
+			## and install OpenMPI (Message Passing Interface)
+			https://wiki.helsinki.fi/display/HUGG/Open+MPI+install+on+Mac+OS+X
+			
+			## try to install Rmpi ... worked!
 		
+## [9] (again) Attempt to install views (only use binaries)
+	ctv::install.views("HighPerformanceComputing")
+	ctv::install.views("Bayesian")					## cairoDevice, gWidgetsRGtk2, AtelieR not installed
+	ctv::install.views("Cluster")					## clusterfly not installed	
+	ctv::install.views("Distributions")				
+	ctv::install.views("Econometrics")
+	ctv::install.views("ExperimentalDesign")
+	ctv::install.views("ExtremeValue")
+	ctv::install.views("Finance")					## Rglpk, Quantlib
+	ctv::install.views("Graphics")
+	ctv::install.views("MachineLearning")			## Rweka failed need JDK
+	
+		## need to instsll a jdk (Java Development Kit) to install RWeka, so download the latest 
 		
+		java version "10.0.1" 2018-04-17
+		Java(TM) SE Runtime Environment 18.3 (build 10.0.1+10)
+		Java HotSpot(TM) 64-Bit Server VM 18.3 (build 10.0.1+10, mixed mode)
 		
+		## Installed the JDK 10.*; but looks like RWeka is looking for version 9.*,
+		## so the install failed
+		  dlopen(/Library/Frameworks/R.framework/Versions/3.5/Resources/library/rJava/libs/rJava.so, 
+			6): Library not loaded: /Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/lib/server/libjvm.dylib
+		
+	
+	ctv::install.views("Multivariate")	
+	ctv::install.views("NumericalMathematics")
+	ctv::install.views("Survival")	
+	ctv::install.views("TimeSeries")
 
 
+##-------------------------------------------------------------------
+## Clean install of Python
+##-------------------------------------------------------------------
+
+## [] Follow the instructions on this website
+	
+	http://docs.python-guide.org/en/latest/starting/install3/osx/
+	
+## [] Install homebrew (package manager for mac os)
+	
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	
+## [] Edit /etc/paths to include /usr/local/sbin in the search patch
+	
+	See:  https://www.architectryan.com/2012/10/02/add-to-the-path-on-mac-os-x-mountain-lion/
+
+## Install python via "% brew install python"
 
 
-ctv::install.views("Bayesian")					## Failed compiles: RGtk2, cairoDevice, cudaBayesreg, gWidgetsRGtk2, AtelieR
-ctv::install.views("Cluster")					## binaries only
-ctv::install.views("Distributions")				## binaries only
-ctv::install.views("Econometrics")
-ctv::install.views("ExperimentalDesign")
-ctv::install.views("ExtremeValue")
-ctv::install.views("Finance")
-ctv::install.views("Graphics")
-ctv::install.views("MachineLearning")			## binaries only
-ctv::install.views("Multivariate")				## binaries only
-ctv::install.views("NumericalMathematics")
-ctv::install.views("Survival")					## binaries only
-ctv::install.views("TimeSeries")
-ctv::install.views("WebTechnologies")
+	Python has been installed as
+		/usr/local/bin/python3
 
-## [9] Test a compile of a source binary
-install.packages("OpenCL")
+	Unversioned symlinks `python`, `python-config`, `pip` etc. pointing to
+	`python3`, `python3-config`, `pip3` etc., respectively, have been installed into
+		/usr/local/opt/python/libexec/bin
 
+	If you need Homebrews Python 2.7 run
+		brew install python@2
 
+	Pip, setuptools, and wheel have been installed. To update them run
+		pip3 install --upgrade pip setuptools wheel
+
+	You can install Python packages with
+		pip3 install <package>
+		
+	They will install into the site-package directory
+		/usr/local/lib/python3.6/site-packages
+
+	See: https://docs.brew.sh/Homebrew-and-Python
+	
+	
+## Add the above path to the python simlink to enable links to python / pip
+	
+	export PATH=/usr/local/opt/python/libexec/bin:$PATH
+	
 
 
 
 
 ##-------------------------------------------------------------------
-## Some packages will only install via source; however, we need to 
-## have compilers installed. I followed the basic steps to install 
-## source packages, but I generally prefer to install binary packages 
-## instead.
+## Notes from previous build attempts
 ##-------------------------------------------------------------------
 
 ##-------------------------------------------------------------------
